d5b4401ab43ea4abb965f93affec372b8a5c7d89
Your Name <you@example.com>
2022-02-01 14:04:34 -0400

    mensaje de subida de cambio

---
+++ application/views/home.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="<?php echo base_url('lib/css/styles.css');?>"
+</head>
+<body>
+<header> 
+    <div class="header">
+        <h1>SISTEMA PARA TRIA</h1>
+        <div class="optionsBar">
+            <p> Venezuela, 1 febrero de 2022</p>
+            <span>|</span>
+            <span class="user"></span>
+            <a><img class="close"src="" alt="Salir del sistema" title="Salir"></a>
+        </div>
+    </div>
+    <nav> 
+        <ul class="cont-ul">
+
+            <li><a>INICIO</a></li>   
+            <li class="user">USUARIO 
+            
+                <ul class="ul-firts">
+                    
+                    <li><a>Nuevo Usuario</a></li>
+                    
+
+                    <li><a> Lista de Usuarios</a></li>
+                </ul>
+            </li>
+        </ul>
+    </nav>
+</header>
+        <section id = "container">
+            <h1>Bienvenidos</h1>
+        </section>
+    
+</body>
+</html>
+        
